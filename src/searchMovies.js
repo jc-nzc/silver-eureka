c4d251e992c9b64491405eb5f3d443358eb798a7
JC <node.zerocool@gmail.com>
2020-12-08 19:44:40 -0600

    component

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function SearchMovies(){
+
+    const searchMovies = async (e) => {
+        e.preventDefault()
+        console.log("submitting");
+    }
+
     return (
-        <form className="form">
-            <label className="label" htmlFor="query">Movie Name </label>
+        <form className="form" onSubmit={searchMovies}>
+            <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Jurassic Park"/>
             <button className="button" type="submit">Search</button>
