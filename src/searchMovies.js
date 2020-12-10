d327a255c0a085cd4207a7fd588da89b93470339
JC <node.zerocool@gmail.com>
2020-12-09 22:03:55 -0600

    api call

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -5,6 +5,14 @@ export default function SearchMovies(){
     const searchMovies = async (e) => {
         e.preventDefault()
         console.log("submitting");
+
+        const query = "Batman";
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=f7608f6360f6fbdaa618b9e61bf22029&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        const res = await fetch(url);
+        const data  = await res.json();
+        console.log(data);
     }
 
     return (
