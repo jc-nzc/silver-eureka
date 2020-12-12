6ef37e7cf5dd86f5dfa7950f0d99c65dd906eb8f
JC <node.zerocool@gmail.com>
2020-12-11 22:29:55 -0600

    searchMovies

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -4,12 +4,15 @@ export default function SearchMovies(){
 
     //states -input query, movies
     const [query, setQuery] = useState('');
+    //create the state for the movies, and update that state appropriately
+    const [movies, setMovies] = useState([]);
+
 
     const searchMovies = async (e) => {
         e.preventDefault()
         // console.log("submitting");
 
-        const query = "One flew over the cuckoo's nest";
+        // const query = "One flew over the cuckoo's nest";
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=f7608f6360f6fbdaa618b9e61bf22029&language=en-US&query=${query}&page=1&include_adult=false`;
 
@@ -19,7 +22,8 @@ export default function SearchMovies(){
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            console.log(data.results);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
@@ -29,7 +33,9 @@ export default function SearchMovies(){
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"/>
+                placeholder="i.e. Jurassic Park"
+                value={query} onChange={(e) => setQuery(e.target.value)}
+                />
             <button className="button" type="submit">Search</button>
         </form>
     )
