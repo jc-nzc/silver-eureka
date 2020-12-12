0122e66c83e8014953a5d05fedc4e1beb33dfc29
JC <node.zerocool@gmail.com>
2020-12-11 22:07:29 -0600

    error handling

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -6,13 +6,20 @@ export default function SearchMovies(){
         e.preventDefault()
         console.log("submitting");
 
-        const query = "Batman";
+        const query = "One flew over the cuckoo's nest";
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=f7608f6360f6fbdaa618b9e61bf22029&language=en-US&query=${query}&page=1&include_adult=false`;
 
-        const res = await fetch(url);
-        const data  = await res.json();
-        console.log(data);
+        // const res = await fetch(url);
+        // const data  = await res.json();
+        // console.log(data);
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
     }
 
     return (
